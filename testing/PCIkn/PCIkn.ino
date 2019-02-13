ab48394855e4bb13260c081cc9c924d5a7def7f5
Teri Ke <32941345+friedforest@users.noreply.github.com>
2019-02-13 12:35:18 -0500

    updt

--- testing/PCIkn/PCIkn.ino
+++ testing/PCIkn/PCIkn.ino
@@ -1,120 +1,102 @@
+/*
+  Example of Wavepacket synthesis, using analog
+  inputs to change the fundamental frequency,
+  bandwidth and centre frequency,
+  using Mozzi sonification library.
+  Demonstrates WavePacket, mozziAnalogRead(), and smoothing
+  control signals with RollingAverage.
+  Also demonstrates AutoMap, which maps unpredictable inputs to a set range.
+  This example goes with a tutorial on the Mozzi site:
+  http://sensorium.github.io/Mozzi/learn/introductory-tutorial/
+  The circuit:
+     Audio output on digital pin 9 on a Uno or similar, or
+    DAC/A14 on Teensy 3.1, or
+     check the README or http://sensorium.github.com/Mozzi/
+     Potentiometer connected to analog pin 0.
+       Center pin of the potentiometer goes to the analog pin.
+       Side pins of the potentiometer go to +5V and ground
+     Light dependent resistor (LDR) and 5.1k resistor on analog pin 1:
+       LDR from analog pin to +5V (3.3V on Teensy 3.1)
+       5.1k resistor from analog pin to ground
+     Light dependent resistor (LDR) and 5.1k resistor on analog pin 2:
+       LDR from analog pin to +5V (3.3V on Teensy 3.1)
+       5.1k resistor from analog pin to ground
+  Mozzi documentation/API
+  https://sensorium.github.io/Mozzi/doc/html/index.html
+  Mozzi help/discussion/announcements:
+  https://groups.google.com/forum/#!forum/mozzi-users
+  Tim Barrass 2013, CC by-nc-sa.
+*/
 
-#include <MozziGuts.h>
-#include <Oscil.h> // oscillator
-#include <tables/cos2048_int8.h> // table for Oscils to play
-#include <Smooth.h>
-#include <AutoMap.h> // maps unpredictable inputs to a range
-#include <PinChangeInt.h>
-
-#define PIN 4
-
-// desired carrier frequency max and min, for AutoMap
-const int MIN_CARRIER_FREQ = 22;
-const int MAX_CARRIER_FREQ = 440;
-
-// desired intensity max and min, for AutoMap, note they're inverted for reverse dynamics
-const int MIN_INTENSITY = 700;
-const int MAX_INTENSITY = 10;
-
-// desired mod speed max and min, for AutoMap, note they're inverted for reverse dynamics
-const int MIN_MOD_SPEED = 10000;
-const int MAX_MOD_SPEED = 1;
-
-AutoMap kMapCarrierFreq(0,1023,MIN_CARRIER_FREQ,MAX_CARRIER_FREQ);
-AutoMap kMapIntensity(0,1023,MIN_INTENSITY,MAX_INTENSITY);
-AutoMap kMapModSpeed(0,1023,MIN_MOD_SPEED,MAX_MOD_SPEED);
+#include <WavePacket.h>
+#include <RollingAverage.h>
+#include <AutoMap.h>
 
 const int KNOB_PIN = 0; // set the input for the knob to analog pin 0
 const int LDR1_PIN=1; // set the analog input for fm_intensity to pin 1
 const int LDR2_PIN=2; // set the analog input for mod rate to pin 2
 
-Oscil<COS2048_NUM_CELLS, AUDIO_RATE> aCarrier(COS2048_DATA);
-Oscil<COS2048_NUM_CELLS, AUDIO_RATE> aModulator(COS2048_DATA);
-Oscil<COS2048_NUM_CELLS, CONTROL_RATE> kIntensityMod(COS2048_DATA);
+// min and max values of synth parameters to map AutoRanged analog inputs to
+const int MIN_F = 20;
+const int MAX_F = 150;
 
-int mod_ratio = 5; // brightness (harmonics)
-long fm_intensity; // carries control info from updateControl to updateAudio
+const int MIN_BW = 20;
+const int MAX_BW = 600;
 
-// smoothing for intensity to remove clicks on transitions
-float smoothness = 0.95f;
-Smooth <long> aSmoothIntensity(smoothness);
+const int MIN_CF = 60;
+const int MAX_CF = 600;
 
-int carrier_freq;
-int mod_freq;
 
+// for smoothing the control signals
+// use: RollingAverage <number_type, how_many_to_average> myThing
+RollingAverage <int, 16> kAverageF;
+RollingAverage <int, 16> kAverageBw;
+RollingAverage <int, 16> kAverageCf;
+// AutoMap adapts to range of input as it arrives
+AutoMap kMapF(0,1023,MIN_F,MAX_F);
+AutoMap kMapBw(0,1023,MIN_BW,MAX_BW);
+AutoMap kMapCf(0,1023,MIN_CF,MAX_CF);
+
+WavePacket <DOUBLE> wavey; // DOUBLE selects 2 overlapping streams
 
 void setup(){
-  pinMode(PIN, INPUT);
-  digitalWrite(PIN, HIGH);
-  PCintPort::attachInterrupt(PIN, changeFreq, RISING);
-  Serial.begin(115200); // set up the Serial output so we can look at the piezo values // set up the Serial output so we can look at the light level
-  
-  startMozzi(); // :))
+  //Serial.begin(9600); // for Teensy 3.1, beware printout can cause glitches
+  Serial.begin(115200);
+  // wait before starting Mozzi to receive analog reads, so AutoRange will not get 0
+  delay(200);
+  startMozzi();
 }
 
 
 void updateControl(){
-  // read the knob
-  int knob_value = mozziAnalogRead(KNOB_PIN); // value is 0-1023
+  int fundamental = mozziAnalogRead(KNOB_PIN)+1;
+  fundamental = kMapF(fundamental);
+  Serial.print("f=");
+  Serial.print(fundamental);
 
-  // map the knob to carrier frequency
-  carrier_freq = kMapCarrierFreq(knob_value);
+  int bandwidth = mozziAnalogRead(LDR1_PIN);
+  bandwidth = kMapBw(bandwidth);
+  Serial.print("\t bw=");
+  Serial.print(bandwidth);
 
-  //calculate the modulation frequency to stay in ratio
-  mod_freq = carrier_freq * mod_ratio;
+  int centre_freq = mozziAnalogRead(LDR2_PIN);
+  centre_freq = kMapCf(centre_freq);
+  Serial.print("\t cf=");
+  Serial.print(centre_freq);
 
-  // set the FM oscillator frequencies
-  aCarrier.setFreq(carrier_freq);
-  aModulator.setFreq(mod_freq);
+  Serial.println();
 
-  // read the light dependent resistor on the width Analog input pin
-  int LDR1_value= mozziAnalogRead(LDR1_PIN); // value is 0-1023
-  // print the value to the Serial monitor for debugging
-  Serial.print("LDR1 = ");
-  Serial.print(LDR1_value);
-  Serial.print("\t"); // prints a tab
-
-  int LDR1_calibrated = kMapIntensity(LDR1_value);
-  Serial.print("LDR1_calibrated = ");
-  Serial.print(LDR1_calibrated);
-  Serial.print("\t"); // prints a tab
-
- // calculate the fm_intensity
-  fm_intensity = ((long)LDR1_calibrated * (kIntensityMod.next()+128))>>8; // shift back to range after 8 bit multiply
-  Serial.print("fm_intensity = ");
-  Serial.print(fm_intensity);
-  Serial.print("\t"); // prints a tab
-
-  // read the light dependent resistor on the speed Analog input pin
-  int LDR2_value= mozziAnalogRead(LDR2_PIN); // value is 0-1023
-  Serial.print("LDR2 = ");
-  Serial.print(LDR2_value);
-  Serial.print("\t"); // prints a tab
-
-  // use a float here for low frequencies
-  float mod_speed = (float)kMapModSpeed(LDR2_value)/1000;
-  Serial.print("   mod_speed = ");
-  Serial.print(mod_speed);
-  kIntensityMod.setFreq(mod_speed);
-
-  Serial.println(); // finally, print a carraige return for the next line of debugging info
+  wavey.set(fundamental, bandwidth, centre_freq);
 }
 
-void changeFreq()
-{
-  if ((carrier_freq != 0) || (mod_freq !=\ 0)){
-    carrier_freq = 0;
-    mod_freq = 0;
-  }
-  // aSin.setFreq(freq); // set the frequency
-}
+
 
 int updateAudio(){
-  long modulation = aSmoothIntensity.next(fm_intensity) * aModulator.next();
-  return aCarrier.phMod(modulation);
+  return wavey.next()>>8; // >>8 for AUDIO_MODE STANDARD
 }
+
 
 
 void loop(){
-  audioHook();
+  audioHook(); // required here
 }
